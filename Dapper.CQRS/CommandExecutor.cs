d31361f8da36bf580a7037b7a337bf7ec2d32106
Caybo Kotzé <caybokotze@gmail.com>
2023-10-14 14:53:15 +0200

    add some xml comments

--- Dapper.CQRS/CommandExecutor.cs
+++ Dapper.CQRS/CommandExecutor.cs
@@ -5,14 +5,44 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Dapper.CQRS;
 
+/// <summary>
+/// Exposes the overloads which execute synchronous and asynchronous commands
+/// </summary>
 public interface ICommandExecutor
 {
+    /// <summary>
+    /// Execute a synchronous command without a result
+    /// </summary>
+    /// <param name="command"></param>
     void Execute(Command command);
+    
+    /// <summary>
+    /// Executes a synchronous command and returns the result
+    /// </summary>
+    /// <param name="command"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     T Execute<T>(Command<T> command);
+    
+    /// <summary>
+    /// Executes a asynchronous command without a result
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
     Task ExecuteAsync(CommandAsync command);
+    
+    /// <summary>
+    /// Executes a asynchronous command and returns a result
+    /// </summary>
+    /// <param name="command"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     Task<T> ExecuteAsync<T>(CommandAsync<T> command);
 }
-    
+
+/// <summary>
+/// Exposes the overloads which execute synchronous and asynchronous commands
+/// </summary>
 public class CommandExecutor : ICommandExecutor
 {
     private readonly IServiceProvider _serviceProvider;
